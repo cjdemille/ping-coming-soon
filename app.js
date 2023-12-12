efa672007b06cebfe45ff912246e858e963806da
cjdemille <cjdemille5@gmail.com>
2023-12-12 14:48:45 -0500

    add empty response

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 
 const invalidText = document.querySelector('.invalid-feedback');
+const emptyText = document.querySelector('.empty')
 const emailInput = document.querySelector('#email')
 const submit = document.querySelector('#submit')
 
 const validateEmail = (e) => {
     e.preventDefault();
     const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if (emailInput.value.match(validRegex)) {
-        invalidText.classList.add('hidden');
-        return true();
+
+    if (emailInput.value.length > 0) {
+        if (emailInput.value.match(validRegex)) {
+            invalidText.classList.add('hidden');
+            emptyText.classList.add('hidden');
+            return true();
+        } else {
+            invalidText.classList.remove('hidden');
+            emptyText.classList.add('hidden');
+            emailInput.classList.add('invalid')
+            return false;
+        }
     } else {
-        invalidText.classList.remove('hidden');
-        emailInput.classList.add('invalid')
-        return false;
+        emptyText.classList.remove('hidden');
     }
+
 }
 
 submit.addEventListener('click', validateEmail)
